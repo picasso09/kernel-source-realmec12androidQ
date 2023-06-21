b1ede29549b9b13eab199ecebafddb6e77575f0f
picasso09 <119201284+picasso09@users.noreply.github.com>
2023-06-21 19:21:26 +0700

    Update mtk_gpufreq_core.h

--- drivers/misc/mediatek/base/power/mt6765/mtk_gpufreq_core.h
+++ drivers/misc/mediatek/base/power/mt6765/mtk_gpufreq_core.h
@@ -42,15 +42,15 @@
  * MT6765 segment_3 : GPU DVFS OPP table Setting
  **************************************************/
 #define SEG3_GPU_DVFS_FREQ0			(960000)	/* KHz */
-#define SEG3_GPU_DVFS_FREQ1			(500000)	/* KHz */
+#define SEG3_GPU_DVFS_FREQ1			(700000)	/* KHz */
 #define SEG3_GPU_DVFS_FREQ2			(400000)	/* KHz */
 
 #define SEG3_GPU_DVFS_VOLT0			(100000)		/* mV x 100 */
-#define SEG3_GPU_DVFS_VOLT1			(70000)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VOLT1			(80000)		/* mV x 100 */
 #define SEG3_GPU_DVFS_VOLT2			(65000)		/* mV x 100 */
 
 #define SEG3_GPU_DVFS_VSRAM0			(100500)		/* mV x 100 */
-#define SEG3_GPU_DVFS_VSRAM1			(87500)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VSRAM1			(88000)		/* mV x 100 */
 #define SEG3_GPU_DVFS_VSRAM2			(87500)		/* mV x 100 */
 
 /**************************************************
